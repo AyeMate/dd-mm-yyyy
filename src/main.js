fb64b305db05581eaaed3245d0691b048d40e3f4
AyeMate <ayemate@quack.host>
2024-11-01 23:13:07 +0100

    commit_message

--- src/main.js
+++ src/main.js
@@ -3,5 +3,5 @@ document.addEventListener('DOMContentLoaded', function() {
     const day = String(now.getDate()).padStart(2, '0');
     const month = String(now.getMonth() + 1).padStart(2, '0');
     const year = now.getFullYear();
-    document.getElementById('date').textContent = `${day}.${month}.${year}`;
+    document.getElementById('dd-mm-yyyy').textContent = `${day}.${month}.${year}`;
 });
